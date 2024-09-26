bd243a3134cf8022fb98433661ebb01434e21e01
senasen2 <senasen2111@gmail.com>
2024-09-26 21:59:47 +0300

    Dosya Okuma İslemleri

--- 18-DosyaOkuma/Program.cs
+++ 18-DosyaOkuma/Program.cs
@@ -1,5 +1,21 @@
-﻿namespace _18_DosyaOkuma
+﻿using System.IO;
+
+namespace _18_DosyaOkuma
 {
+    struct Sehir
+    {
+        public string plakaKodu;
+        public string il;
+        public DateTime CreateDate;
+        public bool isActive;
+    }
+
+    public struct Ilce
+    {
+        public int IlceKodu;
+        public int IlKodu;
+        public string IlceAdi;
+    }
     internal class Program
     {
         static void Main(string[] args)
@@ -22,22 +38,127 @@
              */
 
             #endregion
+            SehirleriOKu();
+            IlceleriOku(); //Geriye Ilceler Listesi Dönsün
+            #region Customer-1000.csv dosyasının okunması ve listelere atılması
 
-            #region Customer-1000.csv dosyasının okunması
+            #endregion
+            var ilceler = IlceleriOku();
+            foreach (var item in ilceler)
+            {
+                Console.WriteLine($"{item.IlceKodu} {item.IlKodu} {item.IlceAdi}");
+            }
+
+            #endregion
+        }
+
+        #region IlceleriOku methodu 
+        private static List<Ilce> IlceleriOku()
+        {
+            string path2 = "c:\\Dosyalar\\ilce.csv";
+            List<Ilce> ilceler = new List<Ilce>();
+            try
+            {
+                string[] satirlar = File.ReadAllLines(path2);
+
+                foreach (var satir in satirlar)
+                {
+                    Ilce ilce = new Ilce(); // Sehir struct 'dan ornek alinmasi
+
+                    var sonuc = satir.Split(','); //Gelen satiri ',' gore ayirir.
+                    #region Eger Tirnaklar gorunmek istenmiyorsa 1. Yol Replace Etmektir.
+                    //ilce.IlceKodu = sonuc[0].Replace('"',' ');
+                    //ilce.IlKodu = sonuc[1].Replace('"', ' '); 
+                    #endregion
+
+                    #region Eger Tirnaklar gorunmek istenmiyorsa 2. Yol int cevirmektir.
+                    ilce.IlceKodu = int.Parse(sonuc[0].Replace('"', ' ').Trim());
+                    ilce.IlKodu = int.Parse(sonuc[1].Replace('"', ' ').Trim());
+                    ilce.IlceAdi = sonuc[1].Replace('"', ' ').Trim();
+                    #endregion
+                    ilce.IlceAdi = sonuc[2];
+
+                    ilceler.Add(ilce);
+
+
+                }
+
+            }
+            catch (PathTooLongException ex)
+            {
+                Console.WriteLine("Dosya yolu cok uzun:" + ex.Message);
+            }
+            catch (FileNotFoundException ex)
+            {
+
+                Console.WriteLine("Hata:Aradiginiz dosyaya su an ulasilamadi:" + ex.Message);
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("Hata:Aradiginiz Klasore ulasilamadi:" + ex.Message);
+
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hat:" + ex.Message);
+
+            }
+
+
+
+            return ilceler;
+        
+    }
+
+        #endregion
+
+        private static void SehirleriOKu()
+        {
             string path = "c:\\Dosyalar\\il.csv";
             //string path2= @"c:\Dosyalar\il.csv";
             //File.ReadAllLines("");
+            List<string> plakalar = new List<string>();
+            List<string> iller = new List<string>();
+            List<Sehir> sehirler = new List<Sehir>();
 
             try
             {
-               string[] satirlar =  File.ReadAllLines(path);
-                foreach (var item in satirlar)
+                string[] satirlar = File.ReadAllLines(path);
+                foreach (var satir in satirlar)
                 {
-                    Console.WriteLine(item);
+                    Sehir sehir = new Sehir(); //sehir struct dan ornek alınması
+                    var sonuc = satir.Split(','); //gelen satırı ',' göre ayırır.
+                    sehir.plakaKodu = sonuc[0];
+                    sehir.il = sonuc[1];
+                    sehir.CreateDate = DateTime.Now;
+                    sehir.isActive = true;
+
+                    #region Amele Yöntemi
+                    //plakalar.Add(sonuc[0]); // ayrılan parçanın 0. elemanı plaka kodu 
+                    //iller.Add(sonuc[1]); // ayrılan parçanın 1.elemanı şehir kodu
+                    //Console.WriteLine(item); 
+                    #endregion
+
+                    #region Modern Yöntem
+                    sehirler.Add(sehir);
+                    #endregion
+
+                    #region Amele Yöntemi
+                    //for (int i = 0; i < plakalar.Count; i++)
+                    //{
+                    //    Console.WriteLine($"{plakalar[i]} => {iller[i]} ");
+                    //} 
+                    #endregion
+
+                }
+                foreach (Sehir sehir in sehirler)
+                {
+                    Console.WriteLine($"{sehir.plakaKodu}=>{sehir.il}\t\tKayit Zamani=>{sehir.CreateDate}\tAktifmi=>{sehir.isActive}");
                 }
             }
             catch (FileNotFoundException ex)
-            {   
+            {
                 Console.WriteLine("Hata:Aradığınız dosyaya şu an ulaşılamadı:" + ex.Message);
             }
             catch (DirectoryNotFoundException ex)
@@ -52,11 +173,6 @@
             {
                 Console.WriteLine("Hata" + ex.Message);
             }
-            #endregion
-
-
-
-            #endregion
         }
     }
 }
--- 18-DosyaOkuma/obj/Debug/net8.0/18-DosyaOkuma.AssemblyInfo.cs
+++ 18-DosyaOkuma/obj/Debug/net8.0/18-DosyaOkuma.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("18-DosyaOkuma")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcc9a3bfab837df82c379e7b814c4f3a413d9328")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+277c9330112f3f56b1e95c1cc8b9fa56b479090e")]
 [assembly: System.Reflection.AssemblyProductAttribute("18-DosyaOkuma")]
 [assembly: System.Reflection.AssemblyTitleAttribute("18-DosyaOkuma")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
